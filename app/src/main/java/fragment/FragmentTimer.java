bd6511b155cc281951e7f0fc4b18766b145685e5
Gia Bao <baotg0502@gmail.com>
2024-10-05 11:11:06 +0700

    Add Timer

--- app/src/main/java/fragment/FragmentTimer.java
+++ app/src/main/java/fragment/FragmentTimer.java
@@ -1,66 +1,141 @@
 package fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.duongnvdssupperclock.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentTimer#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Locale;
+
 public class FragmentTimer extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FragmentTimer() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentTimer.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentTimer newInstance(String param1, String param2) {
-        FragmentTimer fragment = new FragmentTimer();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private TextView timerTextView;
+    private Button startButton, cancelButton;
+    private NumberPicker hourPicker, minutePicker, secondPicker;
+    private CountDownTimer countDownTimer;
+    private boolean isTimerRunning = false;
+    private long timeLeftInMillis = 0;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_timer, container, false);
+
+        timerTextView = view.findViewById(R.id.timerTextView);
+        startButton = view.findViewById(R.id.startButton);
+        cancelButton = view.findViewById(R.id.cancelButton);
+        hourPicker = view.findViewById(R.id.hourPicker);
+        minutePicker = view.findViewById(R.id.minutePicker);
+        secondPicker = view.findViewById(R.id.secondPicker);
+
+        setupPickers();
+        setupButtons();
+
+        return view;
+    }
+
+    private void setupPickers() {
+        hourPicker.setMinValue(0);
+        hourPicker.setMaxValue(23);
+        minutePicker.setMinValue(0);
+        minutePicker.setMaxValue(59);
+        secondPicker.setMinValue(0);
+        secondPicker.setMaxValue(59);
+    }
+
+    private void setupButtons() {
+        startButton.setOnClickListener(v -> {
+            if (isTimerRunning) {
+                pauseTimer();
+            } else {
+                startTimer();
+            }
+        });
+
+        cancelButton.setOnClickListener(v -> cancelTimer());
+    }
+
+    private void startTimer() {
+        if (!isTimerRunning) {
+            long totalTimeInMillis = (hourPicker.getValue() * 3600000L) +
+                    (minutePicker.getValue() * 60000L) +
+                    (secondPicker.getValue() * 1000L);
+            if (totalTimeInMillis > 0) {
+                timeLeftInMillis = totalTimeInMillis;
+                countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        timeLeftInMillis = millisUntilFinished;
+                        updateTimerText();
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        isTimerRunning = false;
+                        updateButtons();
+                        // Here you can add sound or vibration for timer completion
+                    }
+                }.start();
+
+                isTimerRunning = true;
+                updateButtons();
+            }
+        }
+    }
+
+    private void pauseTimer() {
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+        isTimerRunning = false;
+        updateButtons();
+    }
+
+    private void cancelTimer() {
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+        timeLeftInMillis = 0;
+        isTimerRunning = false;
+        updateTimerText();
+        updateButtons();
+    }
+
+    private void updateTimerText() {
+        int hours = (int) (timeLeftInMillis / 3600000);
+        int minutes = (int) ((timeLeftInMillis % 3600000) / 60000);
+        int seconds = (int) ((timeLeftInMillis % 60000) / 1000);
+
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds);
+        timerTextView.setText(timeLeftFormatted);
+    }
+
+    private void updateButtons() {
+        if (isTimerRunning) {
+            startButton.setText("PAUSE");
+            cancelButton.setEnabled(true);
+        } else {
+            startButton.setText("START");
+            if (timeLeftInMillis > 0) {
+                cancelButton.setEnabled(true);
+            } else {
+                cancelButton.setEnabled(false);
+            }
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
     }
 }
